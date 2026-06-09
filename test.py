2f394e75af27e95c4e74c15ddab4e4de446bbaa0
qkd0863 <qkd7183@naver.com>
2026-06-10 08:11:10 +0900

    test

--- test.py
+++ test.py
@@ -7,11 +7,10 @@ N, M = map(int, input().split())
 
 graph = [[] for _ in range(N + 1)]
 
-for _ in range(M):
+for i in range(M):
     a, b, c = map(int, input().split())
     graph[a].append((b, c))
     graph[b].append((a, c))
-
 
 INF = int(1e18)
 dist = [INF] * (N + 1)
@@ -19,18 +18,3 @@ dist[1] = 0
 
 heap = []
 heapq.heappush(heap, (0, 1))
-
-while heap:
-    cost, now = heapq.heappop(heap)
-
-    if dist[now] < cost:
-        continue
-
-    for next_node, next_cost in graph[now]:
-        new_cost = cost + next_cost
-
-        if new_cost < dist[next_node]:
-            dist[next_node] = new_cost
-            heapq.heappush(heap, (new_cost, next_node))
-
-print(dist[N])
